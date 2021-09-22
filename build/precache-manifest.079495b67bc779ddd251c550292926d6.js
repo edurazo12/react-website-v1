54fa5fb6a3f12484069057b7dc8845c91ab09e70
Esteban <edurazo12@gmail.com>
2021-09-21 23:07:29 -0700

    Uploaded react app

---
+++ build/precache-manifest.079495b67bc779ddd251c550292926d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383dfe0104d525f4cbd5548e38dd02b0",
+    "url": "/omwcode/index.html"
+  },
+  {
+    "revision": "ad9e277c0228a4230340",
+    "url": "/omwcode/static/css/main.e88de7fa.chunk.css"
+  },
+  {
+    "revision": "7b17283eadbe7d654b74",
+    "url": "/omwcode/static/js/2.5211adc9.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/omwcode/static/js/2.5211adc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad9e277c0228a4230340",
+    "url": "/omwcode/static/js/main.c30e4554.chunk.js"
+  },
+  {
+    "revision": "429cd5d31dc4c98571d4",
+    "url": "/omwcode/static/js/runtime-main.6d35bc01.js"
+  },
+  {
+    "revision": "2b4151e3b41b4c37087ad27cce2db77e",
+    "url": "/omwcode/static/media/StetchLab-Logo-Primary.2b4151e3.jpg"
+  },
+  {
+    "revision": "11db683bdfd5a2113ca623e1d94bae2f",
+    "url": "/omwcode/static/media/florescompany.11db683b.PNG"
+  }
+]);
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { ReactDOM } from 'react';
 import '../App.css';
 import './HeroSection.css';
 import Morpher from './morpher';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import STRETCH from '../../src/images/StetchLab-Logo-Primary.jpg';
+import FLORES from '../../src/images/florescompany.PNG';
+
+
+
 
 
 function HeroSection() {
@@ -17,14 +17,9 @@ function HeroSection() {
         <Morpher />
       </div>
       <div className="imagescontainer">
-        <a href="https://www.stretchlab.com/"><img className ="stretchlab" src="/images/StetchLab-Logo-Primary.jpg" height="70px" width="50px"></img></a>
-        <a href="https://www.thefloresco.com/"><img className ="thefloresco" src="/images/florescompany.png" height="70px" width="90px"></img></a>
-    
+        <a href="https://www.stretchlab.com/"><img className ="stretchlab" src={STRETCH} height="70px" width="50px"></img></a>
+        <a href="https://www.thefloresco.com/"><img className ="thefloresco" src={FLORES} height="80px" width="100px"></img></a>
     </div>
-      
-
-
-   
     </div>
   );
 } 
